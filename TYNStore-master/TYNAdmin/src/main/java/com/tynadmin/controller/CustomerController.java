bf27da63344e625a97170301198561111ca24166
DDoo <tatuan.hutech@gmail.com>
2022-05-20 01:49:57 +0700

    1h49

--- TYNStore-master/TYNAdmin/src/main/java/com/tynadmin/controller/CustomerController.java
+++ TYNStore-master/TYNAdmin/src/main/java/com/tynadmin/controller/CustomerController.java
@@ -2,11 +2,18 @@ package com.tynadmin.controller;
 
 import com.tynadmin.service.CategoryService;
 import com.tynadmin.service.CustomerService;
+import com.tynadmin.util.FileUploadUtils;
+import com.tynadmin.util.RootPathImageUtils;
+import com.tynentity.Category;
 import com.tynentity.Customer;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -26,4 +33,11 @@ public class CustomerController {
         model.addAttribute("customers", customers);
         return "/customer/home";
     }
+
+    @GetMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("category", new Category());
+        return "/category/form";
+    }
+
 }
